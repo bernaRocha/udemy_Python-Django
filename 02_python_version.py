37c13db12fbd1649ab69515fb44bf327c146a84f
Bernardo <mrochabernardo@gmail.com>
2022-08-12 09:55:45 -0300

    python types

--- 02_python_version.py
+++ 02_python_version.py
@@ -1,4 +1,3 @@
-from email import message
 import platform
 
 version = platform.python_version()
---
+++ 04_types.py
@@ -0,0 +1,5 @@
+x = f'Hello world {1:<03} {1:>03}' ### 100 001
+
+print('x is {}'.format(x))
+
+print(type(x))
